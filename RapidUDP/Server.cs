3bd7ec5ea587785f6984076586ad5584df8dc9d0
alfonjoh <56268011+alfonjoh@users.noreply.github.com>
2022-04-23 17:09:59 +0200

    Add project files.

---
+++ RapidUDP/Server.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Text;
+
+namespace RapidUDP
+{
+    public class Server
+    {
+        private IPAddress Address;
+        private int Port;
+
+        public Server(string addr, int port)
+        {
+            var byteAddr = Encoding.ASCII.GetBytes(addr);
+            Address = new IPAddress(byteAddr);
+            Port = port;
+        }
+    }
+}
